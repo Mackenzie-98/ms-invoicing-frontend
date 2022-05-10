cd6324e20b9c69a929126443fe608eb9a783f39a
mackenzie-98 <adrian.melo@ceiba.com.co>
2022-05-10 14:01:41 -0500

    fix bug

--- src/app/feature/factura/components/crear-factura/crear-factura.component.spec.ts
+++ src/app/feature/factura/components/crear-factura/crear-factura.component.spec.ts
@@ -24,6 +24,7 @@ describe('CrearFacturaComponent', () => {
   let mockRouter = {
     
     navigate: jasmine.createSpy('navigate'),
+    navigateByUrl: jasmine.createSpy('navigateByUrl'),
     getCurrentNavigation: () => {
       return {
          extras: {
